00d22e7d565201bce7b0782ae4903cf6303e2051
alevelalt <alevelalt@gmail.com>
2023-06-28 17:30:46 +0600

    Create server

---
+++ index.js
@@ -0,0 +1,28 @@
+// dependencies
+const http = require("http");
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+  port: 3000,
+};
+
+// create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(
+    app.config.port,
+    console.log(`listening to port ${app.config.port}`)
+  );
+};
+
+// handle Request Response
+app.handleReqRes = (req, res) => {
+  res.end("hello world.");
+};
+
+// start the server
+
+app.createServer();
